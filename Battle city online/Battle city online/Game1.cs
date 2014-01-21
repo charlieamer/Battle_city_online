16c0423b2245b5e0d193879d1732316fc24d8eea
harisbotic <haris.zeznosamte@hotmail.com>
2014-01-21 17:27:45 +0100

    dodao sam sada svoje slike

--- Battle city online/Battle city online/Game1.cs
+++ Battle city online/Battle city online/Game1.cs
@@ -16,7 +16,7 @@ namespace Battle_city_online
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
-
+        
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
