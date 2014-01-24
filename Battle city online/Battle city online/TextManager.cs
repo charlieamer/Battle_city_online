b74dcedfda5d081720eaa1e10b711d513a7840e8
charlieamer <amer_zavlan@live.com>
2014-01-24 22:16:19 +0100

    Dodao Text Manager i TextObject da se sada moze crtati text yaaaaay

---
+++ Battle city online/Battle city online/TextManager.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+namespace Battle_city_online
+{
+    class TextManager
+    {
+        private static TextManager instance;
+        private XmlReader reader;
+        private Dictionary<string, FontData> fonts = new Dictionary<string,FontData>();
+        private Dictionary<string, Texture2D> textures = new Dictionary<string,Texture2D>();
+        private ContentManager contentManager;
+        public static TextManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new TextManager();
+                }
+                return instance;
+            }
+        }
+        private TextManager()
+        {
+            this.contentManager = new ContentManager(ScreenManager.Instance.ServiceProvider);
+            this.LoadFont("workdir/fontovi/cells.xml");
+        }
+
+        private FontData ReadFont(FontData data)
+        {
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name == "font")
+                    {
+                        data = ReadFontData(data);
+                    }
+                }
+            }
+            return data;
+        }
+
+        private FontData ReadFontData(FontData data)
+        {
+            data.name = reader.GetAttribute("name");
+            data.texture = reader.GetAttribute("texture");
+            while (reader.NodeType != XmlNodeType.EndElement ||reader.Name != "font")
+            {
+                reader.Read();
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name == "spacing")
+                    {
+                        data = ReadFontSpacing(data);
+                    }
+                    if (reader.Name == "characters")
+                    {
+                        data.characterMap += reader.GetAttribute("value");
+                    }
+                }
+            }
+            return data;
+        }
+
+        private FontData ReadFontNormalSpacing(FontData data)
+        {
+            while (reader.NodeType != XmlNodeType.EndElement || reader.Name != "spacing")
+            {
+                reader.Read();
+                if (reader.Name == "x")
+                {
+                    while (reader.NodeType != XmlNodeType.Text) reader.Read();
+                    data.spacingX = int.Parse(reader.Value);
+                    while (reader.NodeType != XmlNodeType.EndElement) reader.Read();
+                }
+                if (reader.Name == "y")
+                {
+                    while (reader.NodeType != XmlNodeType.Text) reader.Read();
+                    data.spacingX = int.Parse(reader.Value);
+                    while (reader.NodeType != XmlNodeType.EndElement) reader.Read();
+                }
+            }
+            return data;
+        }
+
+        private FontData ReadFontSpacing(FontData data)
+        {
+            if (reader.GetAttribute("type") == "Constant Number")
+            {
+                data.spacing = FontData.Spacing.CONSTANT_NUMBER;
+                data = ReadFontNormalSpacing(data);
+            }
+            if (reader.GetAttribute("type") == "Constant Dimension")
+            {
+                data.spacing = FontData.Spacing.CONSTANT_DIMENSION;
+                data = ReadFontNormalSpacing(data);
+            }
+            return data;
+        }
+
+        public void LoadFont(string path)
+        {
+            Console.Write("Loading font '" + path + "' ... ");
+            this.reader = XmlReader.Create(path);
+            FontData data = new FontData();
+            data = this.ReadFont(data);
+            this.fonts[data.name] = data;
+            if (!this.textures.ContainsKey(data.texture))
+            {
+                this.textures[data.texture] = this.contentManager.Load<Texture2D>(data.texture);
+            }
+            Console.WriteLine("ok");
+        }
+        
+        public void Draw(string name, string text, Vector2 position, SpriteBatch batch)
+        {
+            Draw(name, text, position, batch, Color.White);
+        }
+
+        public void Draw(string name, string text, Vector2 position, SpriteBatch batch, Color color)
+        {
+            FontData font = this.fonts[name];
+            Texture2D texture = this.textures[font.texture];
+            int width = font.spacingX;
+            int height = font.spacingY;
+            Rectangle rect = new Rectangle();
+            Vector2 currentPosition = new Vector2();
+            currentPosition.X = position.X;
+            currentPosition.Y = position.Y;
+            if (font.spacing == FontData.Spacing.CONSTANT_NUMBER)
+            {
+                width = texture.Width / font.spacingX;
+                height = texture.Width / font.spacingX;
+            }
+            int index=-1;
+            if (font.spacing == FontData.Spacing.CONSTANT_NUMBER)
+            {
+                rect.Width = width;
+                rect.Height = height;
+            }
+            foreach (char c in text)
+            {
+                index++;
+                int pos = font.characterMap.IndexOf(c);
+                if (pos == -1) continue;
+                if (font.spacing == FontData.Spacing.CONSTANT_NUMBER)
+                {
+                    rect.X = (pos % (texture.Width / width)) * width;
+                    rect.Y = (pos / (texture.Width / width)) * width;
+                }
+                currentPosition.X += rect.Width;
+                batch.Draw(texture, currentPosition, rect, color);
+            }
+        }
+    }
+}
---
+++ Battle city online/Battle city online/TextObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace Battle_city_online
+{
+    class TextObject : GameObject
+    {
+        public string Text = "";
+        public Color Color { get; set; }
+        private string fontName;
+        public TextObject(string fontName)
+        {
+            this.fontName = fontName;
+            this.Color = Color.White;
+        }
+        public TextObject(string fontName, Color color)
+        {
+            this.fontName = fontName;
+            this.Color = color;
+        }
+        public override void Draw(SpriteBatch batch)
+        {
+            TextManager.Instance.Draw(this.fontName, this.Text, this.Position, batch, this.Color);
+        }
+    }
+}
